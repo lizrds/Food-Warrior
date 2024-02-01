25a3c698e3b8499ec795f3f533d5d57c242c5c3b
lizrds <creeperbum789@gmail.com>
2024-02-01 17:48:22 +0100

    Pradzia

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -1);
+        rb.angularVelocity = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < 0)
+        {
+            print("fail");
+        }
+    }
+}
